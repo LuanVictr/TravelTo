29d7c4e40c96dd882f1d69ed8ccc6fc06cca9067
LuanVictr <luanvictor2108@gmail.com>
2023-08-10 18:12:49 -0300

    Feat: Iniciando a implementacao da tela de login

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import Provider from './pages/Context/Provider';
+import Provider from './Context/Provider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
